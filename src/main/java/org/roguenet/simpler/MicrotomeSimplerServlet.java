42380a9263fb6ae9862050bbeacc8bfc1579a368
Nathan Curtis <nathan.curtis@gmail.com>
2013-07-24 13:13:14 -0700

    Add easy fetching of microtome parameters.

--- src/main/java/org/roguenet/simpler/MicrotomeSimplerServlet.java
+++ src/main/java/org/roguenet/simpler/MicrotomeSimplerServlet.java
@@ -2,11 +2,15 @@ package org.roguenet.simpler;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.microtome.Library;
 import com.microtome.MicrotomeCtx;
 import com.microtome.Page;
 import com.microtome.core.WritableObject;
+import com.microtome.error.MicrotomeError;
 import com.microtome.json.JsonUtil;
 import com.samskivert.util.Logger;
+import com.samskivert.util.StringUtil;
 import java.io.PrintWriter;
 import java.lang.reflect.Method;
 import react.UnitSignal;
@@ -48,6 +52,23 @@ public class MicrotomeSimplerServlet extends SimplerServlet {
             json = wrapper;
         }
         _gson.toJson(json, out);
+    }
+
+    protected <T extends Page> T mtParam (String name) {
+        String param = stringParam(name);
+        if (StringUtil.isBlank(param)) return null;
+
+        try {
+            JsonObject obj = (JsonObject)new JsonParser().parse(param);
+            Library lib = new Library();
+            _microtome.load(lib, JsonUtil.createReaders(obj));
+            @SuppressWarnings("unchecked")
+            T page = (T)lib.getItem(name);
+            return page;
+        } catch (MicrotomeError me) {
+            log.warning("Error reading Microtome parameter", "json", param, me);
+            return null;
+        }
     }
 
     protected final MicrotomeCtx _microtome;
