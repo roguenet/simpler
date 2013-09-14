d20b9e83126d6819a77a417f90f5fa637830f7e4
Nathan Curtis <nathan.curtis@gmail.com>
2013-09-13 20:37:27 -0700

    Allow streaming Libraries in addition to Pages.
    Complicated Microtome responses that include PageRefs to other bits in a response need to be
    capable of streaming an entire Library to actually pull that off.

--- src/main/java/org/roguenet/simpler/MicrotomeSimplerServlet.java
+++ src/main/java/org/roguenet/simpler/MicrotomeSimplerServlet.java
@@ -6,6 +6,7 @@ import com.google.gson.JsonParser;
 import com.microtome.Library;
 import com.microtome.MicrotomeCtx;
 import com.microtome.Page;
+import com.microtome.core.LibraryItem;
 import com.microtome.core.WritableObject;
 import com.microtome.error.MicrotomeError;
 import com.microtome.json.JsonUtil;
@@ -36,20 +37,26 @@ public class MicrotomeSimplerServlet extends SimplerServlet {
             super.serializeResponse(method, response, out);
             return;
         }
-        if (!(response instanceof Page)) {
-            log.warning("Response must be a subclass of Page, falling back on gson serialization",
-                "method", method.method);
+        if (!(response instanceof Page) && !(response instanceof Library)) {
+            log.warning("Response must be a subclass of Page or Library, falling back on gson " +
+                "serialization", "method", method.method);
             super.serializeResponse(method, response, out);
             return;
         }
 
         JsonObject json = new JsonObject();
         WritableObject writer = JsonUtil.createWriter(method.responseName, json);
-        _microtome.write((Page)response, writer);
-        if (method.responseName != null) {
-            JsonObject wrapper = new JsonObject();
-            wrapper.add(method.responseName, json);
-            json = wrapper;
+        if (response instanceof Page) {
+            _microtome.write((Page)response, writer);
+            if (method.responseName != null) {
+                JsonObject wrapper = new JsonObject();
+                wrapper.add(method.responseName, json);
+                json = wrapper;
+            }
+        } else {
+            for (LibraryItem item : ((Library)response).children()) {
+                _microtome.write(item, JsonUtil.createWriter(item.name(), json));
+            }
         }
         _gson.toJson(json, out);
     }
